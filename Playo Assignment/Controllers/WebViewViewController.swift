999c45dac3265c7561c4e83cef0725afc040c2e7
Rohan Dalmotra <rohandalmotra1@gmail.com>
2022-08-19 19:31:49 +0530

    Starting for a WebView

---
+++ Playo Assignment/Controllers/WebViewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WebViewViewController.swift
+//  Playo Assignment
+//
+//  Created by Rohan Dalmotra on 19/08/22.
+//
+
+import UIKit
+
+class WebViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
