31227f243f9d349542c2562176d300d792f9c6df
Rohan Dalmotra <rohandalmotra1@gmail.com>
2022-08-19 19:54:36 +0530

    Added WebView in ViewContoller

--- Playo Assignment/Controllers/WebViewViewController.swift
+++ Playo Assignment/Controllers/WebViewViewController.swift
@@ -6,24 +6,20 @@
 //
 
 import UIKit
+import WebKit
 
 class WebViewViewController: UIViewController {
+    var urlForWebView = ""
 
+    @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let url = URL (string: urlForWebView)
+                let request = URLRequest(url: url!)
+                webView.load(request)
+       
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
